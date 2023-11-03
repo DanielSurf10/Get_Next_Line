512792dba361bec92cafb61d8642ad9aaaffbe84
Daniel Barbosa Alves <danbarbo@c2r10p4.42sp.org.br>
2023-11-03 20:31:34 -0300

    alguns testes

--- teste/teste7.c
+++ teste/teste7.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   teste6.c                                           :+:      :+:    :+:   */
+/*   teste7.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: danbarbo <danbarbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 13:21:11 by danbarbo          #+#    #+#             */
-/*   Updated: 2023/11/03 19:10:49 by danbarbo         ###   ########.fr       */
+/*   Updated: 2023/11/03 20:30:41 by danbarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,28 @@
 
 int	main()
 {
-	int		fd1;
-	int		fd2;
-	char	*line1;
-	char	*line2;
+	int		fd[2048];
+	char	*line[2048];
+	int		i = 0;
+	int		lines = 0;
 
-	fd1 = open("file3.txt", O_RDONLY);
-	fd2 = open("file3.txt", O_RDONLY);
+	while (i < 2048)
+		fd[i++] = open("file2.txt", O_RDONLY);
 
-	line1 = get_next_line(fd1);
-	line2 = get_next_line(fd2);
-	// printf("line2 = %s\n", line2);
-	// line2 = get_next_line(fd2);
+	// line[0] = get_next_line(fd[0]);
+	line[0] = (char *) 1;
 
-	while (line1 || line2)
+	while (lines < 10)
 	{
-		// printf("line1 = %s\n", line1);
-		// printf("line2 = %s\n", line2);
-		free(line1);
-		free(line2);
-		line1 = get_next_line(fd1);
-		line2 = get_next_line(fd2);
+		i = 0;
+		while (i < 2048)
+		{
+			line[i] = get_next_line(fd[i]);
+			printf("%d = %s", fd[i], line[i]);
+			free(line[i]);
+			i++;
+		}
+		lines++;
 	}
 	return (0);
 }
