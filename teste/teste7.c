f91953c6062621268939fc8e9c869eb73166dd49
DanielSurf10 <71985055+DanielSurf10@users.noreply.github.com>
2023-11-04 16:40:14 +0000

    um teste

--- teste/teste7.c
+++ teste/teste7.c
@@ -6,34 +6,36 @@
 /*   By: danbarbo <danbarbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 13:21:11 by danbarbo          #+#    #+#             */
-/*   Updated: 2023/11/03 20:30:41 by danbarbo         ###   ########.fr       */
+/*   Updated: 2023/11/04 00:28:28 by danbarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
+#define MAXFDS 8192
+
 int	main()
 {
-	int		fd[2048];
-	char	*line[2048];
+	int		fd[MAXFDS];
+	char	*line;
 	int		i = 0;
 	int		lines = 0;
 
-	while (i < 2048)
+	while (i < MAXFDS)
 		fd[i++] = open("file2.txt", O_RDONLY);
 
 	// line[0] = get_next_line(fd[0]);
-	line[0] = (char *) 1;
+	// line[0] = (char *) 1;
 
-	while (lines < 10)
+	while (lines < 20)
 	{
 		i = 0;
-		while (i < 2048)
+		while (i < MAXFDS)
 		{
-			line[i] = get_next_line(fd[i]);
-			printf("%d = %s", fd[i], line[i]);
-			free(line[i]);
+			line = get_next_line(fd[i]);
+			printf("%d = %s", fd[i], line);
+			free(line);
 			i++;
 		}
 		lines++;
